32e05633db56e7de9e2badbd42531f879350596b
yunomi811 <aldova811@gmail.com>
2023-02-25 06:56:43 +0700

    menambahkan post, put, dan delete

--- api.js
+++ api.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser')
 
 const client = require('./connection')
 const app = express()
+
 app.use(bodyParser.json())
 
 app.listen(3100, () => {
@@ -17,12 +18,44 @@ client.connect(err =>{
     }
 })
 
-app.get('/daftar_komponen', (req,res) => {
-    client.query(`Select * from sensor`, (err,result) =>{
+app.get('/sensor', (req,res) => {
+    client.query(`select * from sensor`, (err,result) =>{
         if(!err){
-            res.send(result)
+            res.send(result.rows)
         }
     })
 })
 
+app.post('/sensor', (req,res) =>{
+    const {kode, nama, harga, stok} = req.body
 
+    client.query(`insert into sensor(kode, nama, harga, stok) VALUES ('${kode}', '${nama}', ${harga}, ${stok})`,(err, result) =>{
+        if(!err){
+            console.log('insert success')
+        } else {
+            console.log(err.message)
+        }
+    })
+})
+
+app.put('/sensor/:id', (req,res) =>{
+    const {nama, harga, stok} = req.body
+
+    client.query(`update sensor set nama = '${nama}', harga = ${harga}, stok = ${stok} where id = ${req.params.id}`,(err, result) =>{
+        if(!err){
+            console.log('update success')
+        } else {
+            console.log(err.message)
+        }
+    })
+})
+
+app.delete('/sensor/:id', (req,res) =>{
+    client.query(`delete from sensor where id = ${req.params.id}`,(err, result) =>{
+        if(!err){
+            console.log('delete success')
+        } else {
+            console.log(err.message)
+        }
+    })
+})
